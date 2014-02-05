d12f6bb288a2e4c8a91769b59a1c3a7a91fde8c0
otrack <0track@gmail.com>
2014-02-05 19:50:02 +0100

    [FIX] ensemble

--- ensemble/src/main/java/org/infinispan/ensemble/MenagerieSerializer.java
+++ ensemble/src/main/java/org/infinispan/ensemble/MenagerieSerializer.java
@@ -5,37 +5,38 @@ import org.infinispan.commons.marshall.jboss.GenericJBossMarshaller;
 import java.io.IOException;
 
 /**
+ * // TODO: Document this
  *
- * @author Pierre Sutra
- * @since 6.0
+ * @author otrack
+ * @since 4.0
  */
+public class MenagerieSerializer<T> implements org.menagerie.Serializer<T>{
 
-public class menagerieSerializer<T> implements org.menagerie.Serializer<T>{
+    public MenagerieSerializer(){}
 
-    public menagerieSerializer(){}
-
+    @Override
     public byte[] serialize(T obj) {
         GenericJBossMarshaller marshaller = new GenericJBossMarshaller();
         try {
             return marshaller.objectToByteBuffer(obj);
         } catch (IOException e) {
-            e.printStackTrace();  // TODO: Customise this generated block
+            e.printStackTrace(); // TODO: Customise this generated block
         } catch (InterruptedException e) {
-            e.printStackTrace();  // TODO: Customise this generated block
+            e.printStackTrace(); // TODO: Customise this generated block
         }
         return null;
     }
 
+    @Override
     public T deserialize(byte[] bytes) {
         GenericJBossMarshaller marshaller = new GenericJBossMarshaller();
         try {
             return (T) marshaller.objectFromByteBuffer(bytes);
         } catch (IOException e) {
-            e.printStackTrace();  // TODO: Customise this generated block
+            e.printStackTrace(); // TODO: Customise this generated block
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();  // TODO: Customise this generated block
+            e.printStackTrace(); // TODO: Customise this generated block
         }
         return null;
-
     }
 }
