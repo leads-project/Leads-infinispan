7e96c809a56706836f5fadde85f4e2ace4d63f44
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-03-14 23:13:50 +0100

    some hava doc, defensive checks on construct, don't start the cacheManager already started

--- versioning/src/main/java/org/infinispan/versioning/VersionedCacheFactory.java
+++ versioning/src/main/java/org/infinispan/versioning/VersionedCacheFactory.java
@@ -7,12 +7,14 @@ import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.infinispan.container.versioning.VersionGenerator;
+import org.infinispan.lifecycle.ComponentStatus;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.versioning.impl.VersionedCacheAtomicMapImpl;
 import org.infinispan.versioning.impl.VersionedCacheAtomicTreeMapImpl;
 import org.infinispan.versioning.impl.VersionedCacheHibernateImpl;
 import org.infinispan.versioning.impl.VersionedCacheNaiveImpl;
+import org.jboss.shrinkwrap.api.IllegalArchivePathException;
 
 /**
  * A factory of {@link VersionedCache} instances.
@@ -30,8 +32,22 @@ public class VersionedCacheFactory {
 		startManager();
 	}
 	
-	
-	public VersionedCache newVersionedCache(CacheType cacheType, VersionGenerator generator ,String cacheName) {
+	/**
+	 * Instantiate a {@link VersionedCache} of the specified type.
+	 * 
+	 * @param cacheType one among the available {@link VersioningTechnique} types.
+	 * @param generator the {@link VersionGenerator} to use
+	 * @param cacheName the name of the cache
+	 * @return an instance of {@link VersionedCache}
+	 */
+	public VersionedCache newVersionedCache(VersioningTechnique cacheType, VersionGenerator generator ,String cacheName) {
+		
+		if( generator == null){
+			throw new IllegalArchivePathException("Invalid generator");
+		} else if(cacheName == null) {
+			throw new IllegalArchivePathException("Cache");				
+		}
+		
 		switch (cacheType) {
 		case NAIVE: {
 			return new VersionedCacheNaiveImpl<>(cacheManager.getCache(cacheName), generator, cacheName);
@@ -43,7 +59,7 @@ public class VersionedCacheFactory {
 			 return new VersionedCacheHibernateImpl<>(cacheManager.getCache(cacheName), generator, cacheName);
 		}
 		case SHARDED_TREE:{
-			 return new VersionedCacheAtomicTreeMapImpl(cacheManager.getCache(cacheName), generator, cacheName);
+			 return new VersionedCacheAtomicTreeMapImpl<>(cacheManager.getCache(cacheName), generator, cacheName);
 		       
 		}
 		default:
@@ -52,7 +68,13 @@ public class VersionedCacheFactory {
 		}		
 	}
 	
-	private  void startManager(){  
+	public  void startManager(){  
+		
+		if (cacheManager!=null && cacheManager.getStatus() == ComponentStatus.RUNNING) {
+			logger.info("CacheMAnager already started, nothing to do here");
+			return;
+		}
+		
         try{
             Properties properties = getProperties();
             properties.load(VersionedCacheFactory.class.getClassLoader().getResourceAsStream("config.properties"));
@@ -73,5 +95,10 @@ public class VersionedCacheFactory {
         cacheManager.start();
         logger.info("Cache manager started.");
     }
+	
+	public void stopManager(){
+		cacheManager.stop();
+	    logger.info("Cache manager stopped.");	    	
+	}
 	
 }
