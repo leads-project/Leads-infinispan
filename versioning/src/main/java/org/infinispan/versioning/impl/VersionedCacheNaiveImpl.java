d7c09392766677b283064b90c5c7cbaffcd7e9e8
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-03-17 16:27:40 +0100

    Implement versionMapGet

--- versioning/src/main/java/org/infinispan/versioning/impl/VersionedCacheNaiveImpl.java
+++ versioning/src/main/java/org/infinispan/versioning/impl/VersionedCacheNaiveImpl.java
@@ -2,36 +2,48 @@ package org.infinispan.versioning.impl;
 
 import java.util.Set;
 import java.util.SortedMap;
+import java.util.TreeMap;
 
 import org.infinispan.Cache;
 import org.infinispan.container.versioning.IncrementableEntryVersion;
 import org.infinispan.container.versioning.VersionGenerator;
+import org.infinispan.versioning.utils.version.IncrementableEntryVersionComparator;
 
 /**
- * Implement the Naive multi-versioning technique.
- * In this implementation, all the versions are stored under the same key.
+ * Implement the Naive multi-versioning technique. In this implementation, all
+ * the versions are stored under the same key. This implies there is one cache
+ * per key.
  * 
  * @author valerio.schiavoni@gmail.com
- *
- * @param <K> the type of the key
- * @param <V> the type of the value
+ * 
+ * @param <K>
+ *            the type of the key
+ * @param <V>
+ *            the type of the value
  */
-public class VersionedCacheNaiveImpl<K,V> extends VersionedCacheAbstractImpl<K,V> {
+public class VersionedCacheNaiveImpl<K, V> extends
+		VersionedCacheAbstractImpl<K, V> {
 
 	/**
 	 * 
-	 * @param delegate the super class
-	 * @param generator the version generator
-	 * @param name the name of the cache
+	 * @param delegate
+	 *            the super class
+	 * @param generator
+	 *            the version generator
+	 * @param name
+	 *            the name of the cache
 	 */
 	public VersionedCacheNaiveImpl(Cache<K, ?> delegate,
 			VersionGenerator generator, String name) {
 		super(delegate, generator, name);
 	}
 
+	/**
+	 * Check if a cache with the given name exists.
+	 */
 	@Override
 	public boolean containsKey(Object key) {
-		return delegate.containsKey(key);
+		return delegate.getCacheManager().cacheExists((String) key);
 	}
 
 	@Override
@@ -49,16 +61,23 @@ public class VersionedCacheNaiveImpl<K,V> extends VersionedCacheAbstractImpl<K,V
 		return delegate.keySet();
 	}
 
+	/**
+	 * Get the cache associated with the given key. This returns all the
+	 * versions of this object. This operation is potentially very expensive.
+	 */
 	@Override
 	protected SortedMap versionMapGet(Object key) {
-		throw new UnsupportedOperationException("to be implemented");
-
+		
+		TreeMap map = new TreeMap<IncrementableEntryVersion, V>(
+				new IncrementableEntryVersionComparator());
+		map.putAll(delegate);
+		return map;
 	}
 
 	@Override
 	protected void versionMapPut(Object key, Object value,
 			IncrementableEntryVersion version) {
-		
+
 		throw new UnsupportedOperationException("to be implemented");
 
 	}
