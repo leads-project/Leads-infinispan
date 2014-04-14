cc498b023f66398e987994d7a20d6514a393e98c
otrack <0track@gmail.com>
2014-04-15 01:39:07 +0200

    [FIX] storage now use JBoss marshaller

--- versioning/src/main/java/org/infinispan/versioning/impl/VersionedCacheDummyImpl.java
+++ versioning/src/main/java/org/infinispan/versioning/impl/VersionedCacheDummyImpl.java
@@ -1,11 +1,12 @@
 package org.infinispan.versioning.impl;
 
 import org.infinispan.Cache;
+import org.infinispan.commons.marshall.jboss.GenericJBossMarshaller;
 import org.infinispan.versioning.utils.collections.ShardedTreeMap;
 import org.infinispan.versioning.utils.version.Version;
 import org.infinispan.versioning.utils.version.VersionGenerator;
-import sun.reflect.generics.tree.Tree;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
@@ -18,6 +19,8 @@ import java.util.TreeMap;
  * @since 7.0
  */
 public class VersionedCacheDummyImpl<K,V> extends VersionedCacheAbstractImpl<K,V> {
+
+    GenericJBossMarshaller marshaller = new GenericJBossMarshaller();
 
     public VersionedCacheDummyImpl(Cache cache,
                                   VersionGenerator generator, String cacheName) {
@@ -37,7 +40,7 @@ public class VersionedCacheDummyImpl<K,V> extends VersionedCacheAbstractImpl<K,V
 
     @Override
     protected SortedMap<Version, V> versionMapGet(K key) {
-        TreeMap<Version,V> map = (TreeMap<Version, V>) delegate.get(key);
+        TreeMap<Version,V> map = retrieveTreeMap(key);
         if(map==null)
             return  new ShardedTreeMap<Version, V>();
         return map;
@@ -45,25 +48,51 @@ public class VersionedCacheDummyImpl<K,V> extends VersionedCacheAbstractImpl<K,V
 
     @Override
     protected void versionMapPut(K key, V value, Version version) {
-        TreeMap<Version,V> treeMap = (TreeMap<Version, V>) delegate.get(key);
+        TreeMap<Version,V> treeMap = retrieveTreeMap(key);
         if(treeMap==null)
             treeMap = new TreeMap<Version, V>();
         treeMap.put(version, value);
-        delegate.put(key,treeMap);
+        storeTreeMap(key, treeMap);
     }
 
     @Override
     public void putAll(K key, Map<Version,V> map){
-        TreeMap<Version,V> treeMap = (TreeMap<Version, V>) delegate.get(key);
+        TreeMap<Version,V> treeMap = retrieveTreeMap(key);
         if(treeMap==null)
             treeMap = new TreeMap<Version, V>();
         treeMap.putAll(map);
-        delegate.put(key,treeMap);
+        storeTreeMap(key, treeMap);
     }
 
     @Override
     public boolean containsKey(Object o) {
         return delegate.containsKey(o);
     }
+
+    // PRIVATE METHODS
+
+    private TreeMap<Version,V> retrieveTreeMap(K key){
+        try {
+            byte[] bb = (byte[]) delegate.get(key);
+            if(bb==null)
+                return null;
+            return (TreeMap<Version,V>) marshaller.objectFromByteBuffer(bb);
+        } catch (Exception e) {
+            e.printStackTrace();  // TODO: Customise this generated block
+        }
+        return null;
+    }
+
+    private void storeTreeMap(K k, TreeMap<Version, V> treeMap){
+        try {
+            byte[] bb = marshaller.objectToByteBuffer(treeMap);
+            delegate.put(k,bb);
+        } catch (IOException e) {
+            e.printStackTrace();  // TODO: Customise this generated block
+        } catch (InterruptedException e) {
+            e.printStackTrace();  // TODO: Customise this generated block
+        }
+    }
+
 
 }
