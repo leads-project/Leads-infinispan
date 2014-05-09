1ad509049b30d3f10f59ebb652762a7aae98062e
otrack <0track@gmail.com>
2014-05-09 14:44:52 +0200

    [NEW] SerialExecutor [NEW] KeySpecificListener

---
+++ core/src/main/java/org/infinispan/notifications/KeySpecificListener.java
@@ -0,0 +1,13 @@
+package org.infinispan.notifications;
+
+/**
+ * // TODO: Document this
+ *
+ * @author otrack
+ * @since 4.0
+ */
+public abstract class KeySpecificListener {
+
+    public Object key;
+
+}
