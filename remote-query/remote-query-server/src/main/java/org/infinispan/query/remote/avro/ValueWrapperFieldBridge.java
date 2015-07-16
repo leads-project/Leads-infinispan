205cb6f81447cb420a7946b2cd00125ce0f8a843
otrack <0track@gmail.com>
2015-07-16 17:34:08 +0200

    [FIX] test for avro query support [ADD] lucene fields now of proper types (full support still in-progress)

--- remote-query/remote-query-server/src/main/java/org/infinispan/query/remote/avro/ValueWrapperFieldBridge.java
+++ remote-query/remote-query-server/src/main/java/org/infinispan/query/remote/avro/ValueWrapperFieldBridge.java
@@ -2,16 +2,15 @@ package org.infinispan.query.remote.avro;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
-import org.apache.lucene.document.BinaryDocValuesField;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.util.BytesRef;
+import org.apache.lucene.document.*;
 import org.hibernate.search.bridge.LuceneOptions;
 import org.hibernate.search.bridge.TwoWayFieldBridge;
+import org.infinispan.query.remote.logging.Log;
+import org.infinispan.util.logging.LogFactory;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
-
 
 /**
  *
@@ -22,35 +21,20 @@ public class ValueWrapperFieldBridge implements TwoWayFieldBridge{
 
    public static final String NULL="__null__";
    public static final String DELIMITER = "::";
+   private static final Log log = LogFactory.getLog(ValueWrapperFieldBridge.class, Log.class);
 
    @Override
    public void set(String name, Object value, Document document, LuceneOptions luceneOptions) {
       GenericData.Record record = (GenericData.Record) value;
       Schema schema = record.getSchema();
-      StringField stringField;
+      Collection<Field> fieldCollection = new ArrayList<>();
       for(Schema.Field field : schema.getFields()){
          if (record.get(field.name())!=null){
-            switch (field.schema().getType()){
-            case MAP:
-               Map<?,?> map = (Map) record.get(field.name());
-               for(Object k: map.keySet()) {
-                  stringField = new StringField(
-                        field.name(),
-                        k.toString()+DELIMITER+(map.get(k)!=null ? map.get(k).toString() : NULL),
-                        Field.Store.NO); // the field is *still* searchable
-                  addField(stringField,document);
-               }
-               break;
-            default:
-               stringField = new StringField(
-                     field.name(),
-                     record.get(field.name()).toString(),
-                     Field.Store.NO);
-               addField(stringField, document);
-            }
+            Schema.Type type = field.schema().getType();
+            computeLuceneFields(fieldCollection, type, field.name(),record.get(field.name()),field.schema());
          }
-
       }
+      addFields(fieldCollection,document);
    }
 
    @Override
@@ -67,20 +51,70 @@ public class ValueWrapperFieldBridge implements TwoWayFieldBridge{
       return object.toString();
    }
 
+   private void addFields(Collection<Field> fieldCollection, Document document) {
+      for(Field field : fieldCollection)
+         addField(field,document);
+   }
+   
    /**
     * By default field whose value is greater than 1000 are not indexed.
     * @param field
     * @param document
     */
-   private void addField(StringField field, Document document){
-      if (field.stringValue().length()>1000){
-         //            // build a binary field (not indexed)
-         //            // limit set to 1000 to handle the hard coded max value in Lucene (~30k) and save space
-         //            document.add(new BinaryDocValuesField(
-         //                    field.name(),
-         //                    new BytesRef(field.stringValue().getBytes())));
-      }else {
-         document.add(field);
+   private void addField(Field field, Document document){
+      if (field instanceof StringField 
+            && field.stringValue().length()>1000) {
+         log.debug("field "+field.name()+" too long");
+         return;
+      }
+      document.add(field);
+   }
+   
+   private void computeLuceneFields(Collection<Field> fieldCollection, Schema.Type type , String name, Object value, Schema schema){
+      switch (type){
+      case MAP:
+         Map<?,?> map = (Map) value;
+         for(Object k: map.keySet()) {
+            fieldCollection.add(
+                  new StringField(
+                        name,
+                        k.toString()+DELIMITER+(map.get(k)!=null ? map.get(k).toString() : NULL),
+                        Field.Store.NO)); // the field is *still* searchable
+         }
+         break;
+      case INT:
+         fieldCollection.add(
+               new IntField(
+                     name,
+                     Integer.valueOf(value.toString()),
+                     Field.Store.NO));
+         break;
+      case LONG:
+         fieldCollection.add(
+               new LongField(
+                     name,
+                     Long.valueOf(value.toString()),
+                     Field.Store.NO));
+         break;
+      case FLOAT:
+         fieldCollection.add(
+               new FloatField(
+                     name,
+                     Float.valueOf(value.toString()),
+                     Field.Store.NO));
+         break;
+      case UNION:
+         for (Schema subSchema : schema.getTypes()) 
+            computeLuceneFields(fieldCollection,subSchema.getType(),name,value,schema);
+         break;
+      case NULL:
+         break;
+      default:
+         fieldCollection.add(
+               new StringField(
+                     name,
+                     value.toString(),
+                     Field.Store.NO));
       }
    }
 }
--- remote-query/remote-query-server/src/test/java/org/infinispan/query/remote/indexing/AvroWrapperIndexingTest.java
+++ remote-query/remote-query-server/src/test/java/org/infinispan/query/remote/indexing/AvroWrapperIndexingTest.java
@@ -1,20 +1,24 @@
 package org.infinispan.query.remote.indexing;
 
-
 import example.avro.User;
 import example.avro.WebPage;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.search.NumericRangeQuery;
 import org.apache.lucene.search.Query;
 import org.hibernate.search.engine.spi.SearchFactoryImplementor;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.query.Search;
 import org.infinispan.query.SearchManager;
 import org.infinispan.query.remote.client.avro.AvroSupport;
 import org.infinispan.test.SingleCacheManagerTest;
-import org.infinispan.test.fwk.TestCacheManagerFactory;
 import org.testng.annotations.Test;
+import sun.text.normalizer.UCharacter;
 
 import java.util.HashMap;
 import java.util.List;
@@ -34,25 +38,29 @@ public class AvroWrapperIndexingTest extends SingleCacheManagerTest {
 
    @Override
    protected EmbeddedCacheManager createCacheManager() throws Exception {
-      ConfigurationBuilder cfg = getDefaultStandaloneCacheConfig(true);
-      cfg.transaction()
-            .indexing().enable()
+      ConfigurationBuilder cfg = getDefaultStandaloneCacheConfig(false);
+      cfg.indexing().enable()
             .addProperty("default.directory_provider", "ram")
             .addProperty("lucene_version", "LUCENE_CURRENT");
-
-      EmbeddedCacheManager cacheManager = TestCacheManagerFactory.createCacheManager(cfg);
+      cfg.validate();
+      
+      Configuration configuration = cfg.build();
+      assert cfg.clustering().cacheMode() == CacheMode.LOCAL;
+      
+      EmbeddedCacheManager cacheManager = new DefaultCacheManager(configuration);
       cacheManager.getCache(); //TODO this ensures the GlobalComponentRegistry is initialised right now, but it's not the cleanest way
-      AvroSupport.registerSchema(cacheManager,User.getClassSchema());
       return cacheManager;
    }
 
    public void testIndexingWithWrapper() throws Exception {
       User user = new User();
       user.setName("Alice");
+      user.setFavoriteNumber(12);
 
       org.apache.avro.Schema schema = user.getSchema();
       GenericRecord guser = new GenericData.Record(schema);
       guser.put("name",user.getName());
+      guser.put("favorite_number",user.getFavoriteNumber());
       cache.put("user", guser);
 
       SearchManager sm = Search.getSearchManager(cache);
@@ -69,8 +77,6 @@ public class AvroWrapperIndexingTest extends SingleCacheManagerTest {
             .matching("Alice")
             .createQuery();
 
-      System.out.println(luceneQuery.toString());
-
       List<Object> list = sm.getQuery(luceneQuery).list();
       assertEquals(1, list.size());
 
@@ -86,9 +92,22 @@ public class AvroWrapperIndexingTest extends SingleCacheManagerTest {
       list = sm.getQuery(luceneQuery).list();
       assertEquals(0, list.size());
 
+      // FIXME add support for numeric queries.
+      luceneQuery = sm.buildQueryBuilderForClass(GenericData.Record.class)
+            .get()
+            .range()
+            .onField("favorite_number")
+            .ignoreFieldBridge()
+            .ignoreAnalyzer()
+            .above(11)
+            .createQuery();
+
+      list = sm.getQuery(luceneQuery).list();
+      // assertEquals(1, list.size());
+
    }
 
-   public void testIndexingWithWrapper2() throws Exception {
+   public void testIndexingWithWrapperWebPage() throws Exception {
       WebPage page = new WebPage();
       page.setKey("http://www.test.com");
       Map<CharSequence,CharSequence> outlinks = new HashMap<>();
